44e83cd3662323f493f7f9f81f472a8d6a747652
Christian Bitschnau <christian.bitschnau@student.uibk.ac.at>
2014-03-07 17:19:57 +0100

    added some basic infrastructure and comments on what to do
    the algorithm in Crawler.java is taken from lecture-7-crawling.pdf

---
+++ ldhomework/src/main/java/ld/ldhomework/Main.java
@@ -0,0 +1,22 @@
+package ld.ldhomework;
+
+import ld.ldhomework.crawler.Crawler;
+
+/**
+ * This is a crawler that does a hop-2 breadth-first search of
+ * http://www.w3.org/People/Berners-Lee/card.rdf, where card.rdf is hop-2
+ * 
+ * Used frameworks:
+ * 
+ * http://code.google.com/p/nxparser or http://jena.apache.org/
+ * 
+ * Submit by e-mail not later than 2014-03-31.
+ * 
+ * @author Christian Bitschnau
+ */
+public class Main {
+	public static void main(String[] args) {
+		Crawler crawler = new Crawler();
+		crawler.crawl();
+	}
+}
