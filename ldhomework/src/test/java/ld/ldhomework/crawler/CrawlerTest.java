3040d7af744c8979243a4cfdc6d73fb134c2c457
Christian Bitschnau <christian.bitschnau@student.uibk.ac.at>
2014-03-08 01:30:03 +0100

    added junit4 added apache jena for rdf parsing

---
+++ ldhomework/src/test/java/ld/ldhomework/crawler/CrawlerTest.java
@@ -0,0 +1,15 @@
+package ld.ldhomework.crawler;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class CrawlerTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+	
+}
