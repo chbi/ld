9cbf7a0305bfe87af2f4a628ee8afdf46eee8774
Christian Bitschnau <christian.bitschnau@student.uibk.ac.at>
2014-03-29 15:04:03 +0100

    added test data for unit test and tryed using the parser in a junit test

---
+++ ldhomework/src/test/java/ld/ldhomework/crawler/NxparserTest.java
@@ -0,0 +1,34 @@
+package ld.ldhomework.crawler;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.semanticweb.yars.nx.Node;
+import org.semanticweb.yars.nx.parser.ParseException;
+import org.semanticweb.yars2.rdfxml.RDFXMLParser;
+
+public class NxparserTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void test() throws IOException, ParseException {
+	FileInputStream is = new FileInputStream("card.rdf");
+
+	RDFXMLParser nxp = new RDFXMLParser(is, "http:/drTitan.info");
+
+	Node[] nxx;
+	while (nxp.hasNext()) {
+
+	    nxx = nxp.next();
+
+	    System.out.println(nxx[0].toN3());
+	    System.out.println("\t" + nxx[1].toN3());
+	    System.out.println("\t\t" + nxx[2].toN3());
+	}
+    }
+}
