b70ff6e08daa437a1889a7274e7635c418427eee
worohregger <wolfgang.rohregger@gmail.com>
2014-03-30 12:44:16 +0200

    Added Triple.equals()

--- ldhomework/src/main/java/ld/ldhomework/crawler/Triple.java
+++ ldhomework/src/main/java/ld/ldhomework/crawler/Triple.java
@@ -3,9 +3,12 @@ package ld.ldhomework.crawler;
 import org.semanticweb.yars.nx.Node;
 
 public class Triple {
-    private TripleEntry subject;
-    private TripleEntry predicate;
+
     private TripleEntry object;
+
+    private TripleEntry predicate;
+
+    private TripleEntry subject;
 
     public Triple(Node subject, Node predicate, Node object) {
 	this.subject = new TripleEntry(subject);
@@ -13,21 +16,75 @@ public class Triple {
 	this.object = new TripleEntry(object);
     }
 
-    public TripleEntry getSubject() {
-	return subject;
-    }
+    @Override
+    public boolean equals(Object obj) {
+	boolean result = false;
 
-    public TripleEntry getPredicate() {
-	return predicate;
+	if (obj instanceof Triple) {
+	    Triple otherTriple = (Triple) obj;
+
+	    TripleEntry otherSubject = otherTriple.getSubject();
+	    TripleEntry otherPredicate = otherTriple.getPredicate();
+	    TripleEntry otherObject = otherTriple.getObject();
+
+	    boolean isSameSubject = isSameSubject(otherSubject);
+	    boolean isSamePredicate = isSamePredicate(otherPredicate);
+	    boolean isSameObject = isSameObject(otherObject);
+
+	    result = isSameSubject && isSamePredicate && isSameObject;
+	}
+
+	return result;
     }
 
     public TripleEntry getObject() {
 	return object;
     }
 
-    public TripleEntry[] toTripleEntryArray() {
-	TripleEntry[] array = { subject, predicate, object };
-	return array;
+    public TripleEntry getPredicate() {
+	return predicate;
+    }
+
+    public TripleEntry getSubject() {
+	return subject;
+    }
+
+    private boolean isSameObject(TripleEntry otherObject) {
+	boolean result = false;
+	TripleEntry ourObject = this.getObject();
+
+	result = isSameTripleEntry(ourObject, otherObject);
+
+	return result;
+    }
+
+    private boolean isSamePredicate(TripleEntry otherPredicate) {
+	boolean result = false;
+	TripleEntry ourPredicate = this.getPredicate();
+
+	result = isSameTripleEntry(ourPredicate, otherPredicate);
+
+	return result;
+    }
+
+    private boolean isSameSubject(TripleEntry otherSubject) {
+	boolean result = false;
+	TripleEntry ourSubject = this.getSubject();
+
+	result = isSameTripleEntry(ourSubject, otherSubject);
+
+	return result;
+    }
+
+    private boolean isSameTripleEntry(TripleEntry tripleEntry1,
+	    TripleEntry tripleEntry2) {
+	boolean result = false;
+	if (tripleEntry1 == null && tripleEntry2 == null) {
+	    result = true;
+	} else if (tripleEntry1 != null) {
+	    result = tripleEntry1.equals(tripleEntry2);
+	}
+	return result;
     }
 
     @Override
@@ -36,5 +93,9 @@ public class Triple {
 		+ object.getValue() + "]";
     }
 
+    public TripleEntry[] toTripleEntryArray() {
+	TripleEntry[] array = { subject, predicate, object };
+	return array;
+    }
 
 }
